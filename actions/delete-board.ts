5ccd75f3f29b0aba4d74d96d6e546812cc14aa81
xiaohuigojo <125671000+dengxinyeye@users.noreply.github.com>
2024-03-02 23:16:44 +0800

    Initial commit

---
+++ actions/delete-board.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function deleteBoard(id: string) {
+  await db.board.delete({
+    where: {
+      id,
+    },
+  });
+
+  revalidatePath("/organization/org_2Zk9gkZkzK9IGeLoCA36q8qNyqG?title=resr");
+}
